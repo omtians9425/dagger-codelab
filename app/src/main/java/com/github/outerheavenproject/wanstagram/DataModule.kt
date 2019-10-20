e3af3bd241469e59d4a0a52c208954d53c947257
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-20 22:59:30 +0900

    implement module

---
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/DataModule.kt
@@ -0,0 +1,28 @@
+package com.github.outerheavenproject.wanstagram
+
+import com.github.outerheavenproject.wanstagram.data.DogService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import kotlinx.serialization.json.Json
+import retrofit2.Retrofit
+import javax.inject.Singleton
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.create
+
+@Module
+class DataModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/")
+            .addConverterFactory(
+                Json.asConverterFactory("application/json".toMediaType())
+            )
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideDogService(retrofit: Retrofit) : DogService = retrofit.create()
+}
