0fdf4321adf538936d1a28c6a3a49e1c82b8ea38
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-20 23:05:12 +0900

    add module class to object graph

--- app/src/main/java/com/github/outerheavenproject/wanstagram/AppComponent.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/AppComponent.kt
@@ -4,10 +4,11 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(modules = [DataModule::class])
 interface AppComponent {
     @Component.Factory
     interface Factory {
+        //Not Need to pass arguments because DataModule has public constructor without arguments
         fun create(): AppComponent
     }
 }
