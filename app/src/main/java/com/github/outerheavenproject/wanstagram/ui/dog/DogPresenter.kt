3b7bece74b17d56a463acde157be6f42d457244b
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-20 23:12:51 +0900

    modify DogPresenter to DI version

--- app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenter.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenter.kt
@@ -1,14 +1,27 @@
 package com.github.outerheavenproject.wanstagram.ui.dog
 
+import com.github.outerheavenproject.wanstagram.data.DogService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.github.outerheavenproject.wanstagram.data.getDogService
+import javax.inject.Inject
 
-class DogPresenter(
-    private val view: DogContract.View
+/**
+ * This annotation specifies
+ * 1. Implemented instance of DogService is injected to This Object (as long as instance is resolved in the object graph).
+ * 2. DogPresenter is registered in the object graph.
+ */
+class DogPresenter @Inject constructor(
+    private val dogService: DogService
 ) : DogContract.Presenter {
+    private lateinit var view: DogContract.View
+
+    fun attachView(view: DogContract.View) {
+        this.view = view
+    }
+
     override suspend fun start() {
-        val dogs = getDogService().getDogs(limit = 20)
+        val dogs = dogService.getDogs(limit = 20)
         withContext(Dispatchers.Main) {
             view.updateDogs(dogs)
         }
