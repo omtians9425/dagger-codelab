df211065fc016248c936e78a9bdfb2eeabbe8197
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-21 00:05:36 +0900

    create mock class

--- app/src/test/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenterTest.kt
+++ app/src/test/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenterTest.kt
@@ -1,6 +1,9 @@
 package com.github.outerheavenproject.wanstagram.ui.dog
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.outerheavenproject.wanstagram.data.Dog
+import com.github.outerheavenproject.wanstagram.data.DogService
+import com.github.outerheavenproject.wanstagram.data.Dogs
 import org.junit.Before
 import org.junit.Test
 
@@ -16,5 +19,26 @@ class DogPresenterTest {
 
     @Test
     fun start() {
+    }
+}
+
+private class TestDogService : DogService {
+    override suspend fun getDog(): Dog {
+        return Dog(url = "1", status = "success")
+    }
+
+    override suspend fun getDogs(limit: Int): Dogs {
+        return Dogs(urls = listOf("1"), status = "success")
+    }
+
+    override suspend fun getBleed(bleed: String, limit: Int): Dogs {
+        return Dogs(urls = listOf("1"), status = "success")
+    }
+}
+
+private class TestView : DogContract.View {
+    var called = 0
+    override fun updateDogs(dogs: Dogs) {
+        called++
     }
 }
