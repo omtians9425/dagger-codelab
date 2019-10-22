4b229b5b8254801e1063d43f66e732861b2dc9f0
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 22:06:56 +0900

    fix duplicate binding error

--- app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogFragmentModule.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogFragmentModule.kt
@@ -1,23 +1,10 @@
 package com.github.outerheavenproject.wanstagram.ui.dog
 
-import com.github.outerheavenproject.wanstagram.ui.AppNavigator
-import com.github.outerheavenproject.wanstagram.ui.AppNavigatorImpl
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface DogFragmentModule {
-    @ContributesAndroidInjector(
-        modules = [
-            DogFragmentBindModule::class
-        ]
-    )
+    @ContributesAndroidInjector
     fun contributeDogFragment(): DogFragment
-}
-
-@Module
-interface DogFragmentBindModule {
-    @Binds
-    fun bindAppNavigator(navigator: AppNavigatorImpl): AppNavigator
 }
