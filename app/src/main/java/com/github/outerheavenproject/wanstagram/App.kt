52668e32dfc05f56f56e09cfb5e5e664265d7673
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 18:37:03 +0900

    change App.kt to implememt androidInjector

--- app/src/main/java/com/github/outerheavenproject/wanstagram/App.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/App.kt
@@ -1,12 +1,21 @@
 package com.github.outerheavenproject.wanstagram
 
 import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class App : Application() {
+class App : Application(), HasAndroidInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
     lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.create()
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 }
