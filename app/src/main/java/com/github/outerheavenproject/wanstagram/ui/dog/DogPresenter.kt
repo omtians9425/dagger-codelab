55fc4c1c8b2d38b86c2d4061a6fd20d07bdb7bc4
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 21:41:53 +0900

    replace DogService with repository at DogPresenter.kt

--- app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenter.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogPresenter.kt
@@ -1,7 +1,6 @@
 package com.github.outerheavenproject.wanstagram.ui.dog
 
-import android.util.Log
-import com.github.outerheavenproject.wanstagram.data.DogService
+import com.github.outerheavenproject.wanstagram.data.DogRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -12,7 +11,8 @@ import javax.inject.Inject
  * 2. DogPresenter is registered in the object graph.
  */
 class DogPresenter @Inject constructor(
-    private val dogService: DogService
+//    private val dogService: DogService
+    private val repository: DogRepository
 ) : DogContract.Presenter {
     private lateinit var view: DogContract.View
 
@@ -21,8 +21,9 @@ class DogPresenter @Inject constructor(
     }
 
     override suspend fun start() {
-        Log.i("DogPresenter", dogService.toString()) //The same instance of ShibaPresenter
-        val dogs = dogService.getDogs(limit = 20)
+//        Log.i("DogPresenter", dogService.toString()) //The same instance of ShibaPresenter
+//        val dogs = dogService.getDogs(limit = 20)
+        val dogs = repository.findAll()
         withContext(Dispatchers.Main) {
             view.updateDogs(dogs)
         }
