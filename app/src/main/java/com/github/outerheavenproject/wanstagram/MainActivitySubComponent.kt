c71993205cc35239822eadf3ee2e6c504c4f40aa
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 16:04:59 +0900

    create subcomponent for UI

---
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/MainActivitySubComponent.kt
@@ -0,0 +1,31 @@
+package com.github.outerheavenproject.wanstagram
+
+import android.content.Context
+import com.github.outerheavenproject.wanstagram.ui.AppNavigator
+import com.github.outerheavenproject.wanstagram.ui.MainActivity
+import com.github.outerheavenproject.wanstagram.ui.dog.DogFragment
+import com.github.outerheavenproject.wanstagram.ui.shiba.ShibaFragment
+import dagger.Binds
+import dagger.BindsInstance
+import dagger.Module
+import dagger.Subcomponent
+
+@Subcomponent
+interface MainActivitySubComponent {
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(
+            @BindsInstance context: Context
+        ): MainActivitySubComponent
+    }
+
+    fun inject(activity: MainActivity): MainActivity
+    fun inject(dogFragment: DogFragment): DogFragment
+    fun inject(shibaFragment: ShibaFragment): ShibaFragment
+}
+
+@Module
+interface MainActivityModule {
+    @Binds
+    fun bindAppNavigator(appNavigator: AppNavigator): AppNavigator
+}
