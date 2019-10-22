d6dee5aee978b34f789c7aaa17f07be60e3c3a22
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 21:42:40 +0900

    change repository to singleton

--- app/src/main/java/com/github/outerheavenproject/wanstagram/data/DogRepository.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/data/DogRepository.kt
@@ -1,7 +1,9 @@
 package com.github.outerheavenproject.wanstagram.data
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class DogRepository @Inject constructor(private val dogService: DogService) {
     companion object {
         private const val LIMIT = 20
