97745cc09cbcc9a635817bc72742bda52ac610f8
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-22 21:38:05 +0900

    create DogRespository

---
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/data/DogRepository.kt
@@ -0,0 +1,17 @@
+package com.github.outerheavenproject.wanstagram.data
+
+import javax.inject.Inject
+
+class DogRepository @Inject constructor(private val dogService: DogService) {
+    companion object {
+        private const val LIMIT = 20
+    }
+
+    private var dogs: Dogs? = null
+    suspend fun findAll(): Dogs {
+        dogs?.let {
+            return it
+        }
+        return dogService.getDogs(LIMIT).also { dogs = it }
+    }
+}
