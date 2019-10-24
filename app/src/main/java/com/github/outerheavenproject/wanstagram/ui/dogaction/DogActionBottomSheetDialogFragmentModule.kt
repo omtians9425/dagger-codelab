a464d14cccbe86630546c96bc4194a27aa79ad05
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-10-24 22:03:24 +0900

    create module for bottomSheetFragment

---
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dogaction/DogActionBottomSheetDialogFragmentModule.kt
@@ -0,0 +1,22 @@
+package com.github.outerheavenproject.wanstagram.ui.dogaction
+
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+interface DogActionBottomSheetDialogFragmentModule {
+    @ContributesAndroidInjector(
+        modules = []
+    )
+    fun contributeDogActionBottomSheetFragment(): DogActionBottomSheetDialogFragment
+}
+
+@Module
+interface DogActionBottomSheetDialogFragmentBindModule {
+    @Binds
+    fun bindView(fragment: DogActionBottomSheetDialogFragment): DogActionBottomSheetContract.View
+
+    @Binds
+    fun bindPresenter(presenter: DogActionBottomSheetPresenter): DogActionBottomSheetContract.Presenter
+}
